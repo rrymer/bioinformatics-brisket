ed269d50f6cd279a788254c14fa6b2f87e182c23
rrymer <pczrr@Richard-Rymers-MacBook-Pro.local>
2014-10-14 16:36:41 -0600

    add interface
    Functions were renamed to better reflect what they are doing, and
    hidden behind an interface: get_taxon_name(…)

--- build-ssn/name_retriever.py
+++ build-ssn/name_retriever.py
@@ -4,26 +4,35 @@ Created on Sat Oct 11 20:13:34 2014
 
 @author: Richard Rymer
 
-Functions to retrieve the formal name for an organism given a taxid.
+Returns the formal name for an organism given a taxid.
 """
 from Bio import Entrez
 import xml.etree.ElementTree as ET
 
-def get_taxid_entry_from_entrez(ID):
+def _retrieve_taxid_entry_from_entrez(ID):
     """
+    Private function
     Retrieves the full record for the taxon with taxid ID, returns the 
     record handle
     """
     return Entrez.efetch(db='taxonomy', id=ID)
 
-def parse_xml(XML):
+def _parse_xml(XML):
     """
+    Private function
     Takes a stream of XML data as a file-like object, and return a parsed object
     """
     return ET.parse(XML)
 
-def get_taxon_name(parsedXMLobject):
+def _retrieve_taxon_name(parsedXMLobject):
     """
-    Takes a parsed XML object, and returns the taxon name
+    Private function
+    Takes a parsed XML object, and extracts and returns the taxon name from the XML object
     """
     return parsedXMLobject.getroot()[0][1].text
+
+def get_taxon_name(ID):
+    """
+    Takes a Taxonomical ID, and returns the taxon name
+    """
+    return _retrieve_taxon_name(_parse_xml(_retrieve_taxid_entry_from_entrez(ID)))
